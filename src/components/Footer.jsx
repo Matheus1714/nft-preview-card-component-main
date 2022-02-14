9c43c52b86d18ce7201a128fa7365d7d500bdcbd
matheus1714 <matheussilvamartins1714@gmail.com>
2022-02-14 14:36:21 -0300

    fix error class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import './Footer.css'
 
 export default () => {
     return (
-        <div class="Footer">
-            <div class="attribution">
+        <div clasName="Footer">
+            <div className="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
                 Coded by <a href="https://github.com/Matheus1714">Matheus Silva Martins Mota</a>.
             </div>
